b4a0da36d579260aa34efd09a1dab8f5aef410b3
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-12 19:13:30 +0330

    define Contact class for save in database
    create contact class with name, email, subject, message, created_date and update_date fields. Also we define __str__ method for show table according name of contact. In class Meta all contact sort according by created_date

--- clothes/models.py
+++ clothes/models.py
@@ -7,3 +7,16 @@ class Category(models.Model):
     def __str__(self):
         return self.category
 
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255, blank=True, null=True)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-created_date']
