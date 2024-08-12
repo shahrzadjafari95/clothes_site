cf35ea83096e3844ca1c58c7223518931bde5adc
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-12 19:19:51 +0330

    define All_Type_clothes
    define All_Type_clothes with  image, price, content, gender, category, status, created_date, update_date and published_date fields for save information in database.

--- clothes/models.py
+++ clothes/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 
-# Create your models here.
+gender_clothes = [('men', 'men'),
+                  ('female', 'female')]
+
+
 class Category(models.Model):
     category = models.CharField(max_length=255)
 
     def __str__(self):
         return self.category
+
+
+class All_Type_Clothes(models.Model):
+    # image = models.ImageField(upload_to='clothes/')
+    price = models.FloatField()
+    content = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255, choices=gender_clothes, default='male')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=False, null=True)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
 
 class Contact(models.Model):
     name = models.CharField(max_length=255)
