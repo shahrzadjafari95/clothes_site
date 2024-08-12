d274871496bdde5f8bc59b816e10e139483847c0
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-12 17:45:09 +0330

    define published_date, created_date and update_date

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,6 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     counted_view = models.BooleanField(default=0)
     status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)  # when the object first created,set current date and time
+    update_date = models.DateTimeField(auto_now=True)  # when the object updated, save current date and time
