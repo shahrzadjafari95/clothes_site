5d7e88a3d0f1a03177fcdfef2cd97a8b69441fbf
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-12 19:34:13 +0330

    define ContactAdmin class for customize admin panel
    for show Contact in admin panel, create ContactAdmin with data_hierarchy, list_display( that contains name, email, created_date fields), list_filter that filters information with created_date, search_fields and ordering fields

--- clothes/admin.py
+++ clothes/admin.py
@@ -4,3 +4,13 @@ from clothes.models import Contact, All_Type_Clothes, Category
 
 
 # Register your models here.
+
+
+class ContactAdmin(admin.ModelAdmin):
+    data_hierarchy = 'created_date'
+    list_display = ['name', 'email', 'created_date']
+    list_filter = ['created_date']
+    search_fields = ('name', 'email')
+    ordering = ['-created_date']
+
+
