1fcdb63c1f11148ba140ac339912cf499d0ce761
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-27 01:54:51 +0330

    update blog_home function
    + get all posts from models.Post
    + filter by status = approved
    + filter posts that published_date great than now time
    + order by posts according last published_date
    + define context for pass items to template and use of them in template

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
+from .models import Post
 
 
 # Create your views here.
 def blog_home(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.filter(status='A', published_date__lte=timezone.now()).order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blog/blog.html', context)
 
 
 def single_blog(reqest):
