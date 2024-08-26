ed577bca31d6729055bc79a4691acba85ae11202
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-27 00:31:24 +0330

    add 'id' field in list_display in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -7,8 +7,8 @@ from blog.models import Post, Category
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    list_display = ('title', 'status', 'author', 'counted_view', 'published_date', 'created_date')
     list_filter = ('status', 'published_date', 'category')
+    list_display = ('title', 'id', 'status', 'author', 'counted_view', 'published_date', 'created_date')
     search_fields = ['title', 'content', 'category']
 
 
