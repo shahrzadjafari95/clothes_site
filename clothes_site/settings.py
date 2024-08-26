7103e68b0c8f1262d26b4ed3d3e59a2844ca2758
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-27 01:50:13 +0330

    add comment for static_root

--- clothes_site/settings.py
+++ clothes_site/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'  # setting for collect statics command
 
 STATICFILES_DIRS = [
     BASE_DIR / "statics",
