7fa0244a82e039733dd988fe3fa2874c0d5455ca
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-27 00:41:33 +0330

    define class StatusFilter for list_filter and filter status in admin panel
    with define and use StatusFilter in admin panel, we can filter status post with check box.

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from blog.models import Post, Category
+from django_admin_filters import MultiChoice
 
 
 # Register your models here.
+
+class StatusFilter(MultiChoice):
+    FILTER_LABEL = "By status"
+
 
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
     list_filter = ('status', 'published_date', 'category')
     list_display = ('title', 'id', 'status', 'author', 'counted_view', 'published_date', 'created_date')
+    list_filter = (('status', StatusFilter),
     search_fields = ['title', 'content', 'category']
 
 
