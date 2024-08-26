eea06df2b2249d49ff673e7bbc8933b13ba9b3a0
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-27 00:00:20 +0330

    add STATUS_CHOICES for status in Post table

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 
 # Create your models here.
+STATUS_CHOICES = (
+    ('P', 'Pending'),
+    ('A', 'Approved'),
+    ('R', 'Rejected'),
+)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
