db1b556642c111c7d207503dbd84395381508752
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-09-11 20:51:30 +0330

    define simple template tag count_view for count views of each post in blog

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,2 +1,13 @@
 from django import template
+from django.utils import timezone
+from blog.models import Post, Category
 register = template.Library()
+
+
+@register.simple_tag
+def count_view(pid):
+    posts = Post.objects.filter(status='A', published_date__lte=timezone.now())
+    post = get_object_or_404(posts, pk=pid)
+    post.counted_view += 1
+    post.save()
+    return ''
