697fb20b4faf8c49fba04f8e5e9172e7de24fabd
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-09-11 21:23:18 +0330

    define categories inclusion tag for display count of each categories that use each post

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count, Q
+from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from blog.models import Post, Category
 
@@ -24,4 +26,13 @@ def latest_post(arg=3):
 def popular_post(arg):
     posts = Post.objects.filter(status='A', published_date__lte=timezone.now()).order_by('-counted_view')[:arg]
     return {'posts': posts}
+
+
+@register.inclusion_tag('blog/categories.html')
+def categories(status='A'):
+    all_categories = Category.objects.annotate(post_count=Count('posts', filter=Q(posts__status=status) &
+                    Q(posts__published_date__lte=timezone.now()))).filter(post_count__gt=0).order_by('-post_count')  # Only show categories with posts
+    return {'categories': all_categories}
+
+
 
