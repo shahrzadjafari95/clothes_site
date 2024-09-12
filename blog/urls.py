25c5f6fb97712a1f3730d044553b8bffad65e63f
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-09-12 11:56:46 +0330

    add category url in blog.urls.py
    + path('category/<str:cat_name>/', views.blog_home, name='category')

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [path('', views.blog_home,  name='blog-home'),
                path('post-<int:pid>', views.single_blog, name='single-blog'),
-
+               path('category/<str:cat_name>/', views.blog_home, name='category'),
 ]
