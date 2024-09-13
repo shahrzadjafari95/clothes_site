ef9f65d27b57d21b3909172db4356175578dab5e
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-09-14 02:15:13 +0330

    update blog_tags.py and single-blog.html
    delete count_view that is a simple tag from blog_tags and remove count_view from single-blog.html

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -6,16 +6,6 @@ from blog.models import Post, Category
 
 register = template.Library()
 
-
-@register.simple_tag
-def count_view(pid):
-    posts = Post.objects.filter(status='A', published_date__lte=timezone.now())
-    post = get_object_or_404(posts, pk=pid)
-    post.counted_view += 1
-    post.save()
-    return ''
-
-
 @register.inclusion_tag('blog/recent-post.html')
 def latest_post(arg=3):
     posts = Post.objects.filter(status='A', published_date__lte=timezone.now()).order_by('-published_date')[:arg]
