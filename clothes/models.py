152d24d91deb2d6250b148b89c91b45ddba8d83f
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-09-30 10:33:58 +0330

    remove def __str__(self) from Contact model

--- clothes/models.py
+++ clothes/models.py
@@ -60,8 +60,5 @@ class Contact(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         ordering = ['-created_date']
