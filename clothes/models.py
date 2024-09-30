5f4cb3b7f17fcca8e0bb2c971050aa3ed26633a8
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-09-30 10:32:01 +0330

    Added image resizing functionality to `save` method for `All_Type_Clothes` model.
    - Resized uploaded images to 230x300 dimensions before saving
    - Used `PIL.Image` to handle image processing.
    - Stored the resized image using `InMemoryUploadedFile` to replace the original image in the model.
    - Ensured high-quality JPEG output with 100% quality.

--- clothes/models.py
+++ clothes/models.py
@@ -1,3 +1,7 @@
+from io import BytesIO
+import sys
+from PIL import Image
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
 
 gender_clothes = [('male', 'male'),
@@ -27,6 +31,21 @@ class All_Type_Clothes(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(null=True)
+
+    def save(self):  # this method resized all image
+        im = Image.open(self.image)  # Opening the uploaded image
+        output = BytesIO()
+        im = im.resize((230, 300))  # Resize/modify the image
+        im.save(output, format='JPEG', quality=100)  # after modifications, save it to the output
+        output.seek(0)
+        # change the imagefield value to be the newly modified image value
+        self.img = InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % self.image.name.split('.')[0], 'image/jpeg',
+                                        sys.getsizeof(output), None)
+
+        super(All_Type_Clothes, self).save()
+
+    def __str__(self):
+        return self.gender
 
     class Meta:
         ordering = ['-created_date']
