03cb8e270958e13fcf93474e2af94b9a9e6f6a61
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-09-30 10:55:27 +0330

    Added dynamic category-based routing for men's and women's sections.
    - Implemented URL patterns for women's category view (`women/<str:cat_name>`).
    - view dynamically handles categories passed through the URL (`cat_name` parameter).

--- clothes/urls.py
+++ clothes/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [path('', views.index,  name='home_page'),
                path('contact/', views.contact, name='contact'),
                path('men-clothes/', views.men, name='men-clothes'),
                path('women-clothes/', views.women, name='women-clothes'),
-
+               path('women/<str:cat_name>', views.women, name='women_category'),
+               path('men/<str:cat_name>/', views.men, name='men_category'),
 ]
