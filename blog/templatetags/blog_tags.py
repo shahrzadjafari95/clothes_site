b7fbf11d6f697bd1a23b47a2ccc1bdef411ae326
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-01 20:22:52 +0330

    update blog_tags.py
    remove from django.shortcuts import get_object_or_404 from blog/templatetags/blog_tags.py

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,5 @@
 from django import template
 from django.db.models import Count, Q
-from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from blog.models import Post, Category
 
