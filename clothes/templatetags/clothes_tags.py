6064c3a4c42dd8c56bb561ca7aea43e3ef39ac73
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-01 20:53:18 +0330

    - If the price is an integer, it removes the decimal point and returns it as an integer.

--- clothes/templatetags/clothes_tags.py
+++ clothes/templatetags/clothes_tags.py
@@ -5,3 +5,6 @@ register = template.Library()
 
 @register.filter
 def format_price(value):
+    # Check if the price is a whole number
+    if value.is_integer():
+        return int(value)  # Remove the decimal point and return as an integer
