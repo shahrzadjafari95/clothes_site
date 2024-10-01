4fcda0366e9d370319d6082aaaeac6cbc77cbbe1
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-01 20:54:13 +0330

    For non-integer prices, it formats the price to two decimal places (e.g., `10.50`).
    - This filter ensures prices are displayed cleanly in templates, improving user experience.

--- clothes/templatetags/clothes_tags.py
+++ clothes/templatetags/clothes_tags.py
@@ -8,3 +8,4 @@ def format_price(value):
     # Check if the price is a whole number
     if value.is_integer():
         return int(value)  # Remove the decimal point and return as an integer
+    return f'{value:.2f}'  # Return the value formatted to 2 decimal places
