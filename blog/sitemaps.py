90bbeb92768e9365e182fe21f90087e5fcede24d
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-02 12:52:25 +0330

    Implement 'items()' method
      - This method fetches all blog posts from the 'Post' model where 'status' is "A" (active) and the 'published_date' is less than or equal to the current time. This ensures that only published blog posts are included in the sitemap.
    - Import 'timezone' from 'django.utils' to handle timezone-aware queries.

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,4 +1,6 @@
 from django.contrib.sitemaps import Sitemap
+from django.utils import timezone
+
 from blog.models import Post
 
 
@@ -6,3 +8,7 @@ class BlogSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.5
 
+    def items(self):  # The items() method determines what data or objects will be included in the sitemap.
+
+        # This method queries the Post model to get all posts where the status is "A" and published_date less than now
+        return Post.objects.filter(status="A", published_date__lte=timezone.now())
