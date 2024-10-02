b40fd3bf313332b65ac21ee1351a73a7821521de
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-02 12:38:01 +0330

    add     'django.contrib.sites', in installed app

--- clothes_site/settings.py
+++ clothes_site/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django_extensions',
+    'django.contrib.sites',
     'clothes',
     'blog',
     'rangefilter',
