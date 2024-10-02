b4293f0efc454a941a90ed110c28d673e879df3b
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-02 12:47:10 +0330

    Implement 'items()' method returning a list of static view names.
    This method lists the URL names for static pages

--- clothes/sitemaps.py
+++ clothes/sitemaps.py
@@ -3,3 +3,6 @@ class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
     changefreq = "daily"
 
+    # This method returns a list of URL names (defined in your urls.py) that should be included in the sitemap.
+    def items(self):
+        return ["clothes:home_page", "clothes:men-clothes", "clothes:women-clothes", "clothes:contact"]
