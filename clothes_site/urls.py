e345b6f3da32456e74e596bd1dfe187aa8da0c0a
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-02 12:57:04 +0330

    Define URL patterns for the sitemap
      - The path "sitemap.xml" is mapped to the 'sitemap' view. It takes the 'sitemaps' dictionary as an argument to determine which sitemaps to include in the XML response.
    
      - The name of this route is set to "django.contrib.sitemaps.views.sitemap", which can be used for URL reversing.

--- clothes_site/urls.py
+++ clothes_site/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('clothes.urls')),
     path('blog/', include('blog.urls')),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
