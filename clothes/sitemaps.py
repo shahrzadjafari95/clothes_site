a4d5958f67a1016fc501c1da33129ed7573e1f1a
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-02 12:47:55 +0330

    - Implement 'location()' method using 'reverse()' to retrieve the actual URL for each item.
    The 'location()' method takes each item (URL name) returned by 'items()' and converts it into a full URL using Django's 'reverse()' function.

--- clothes/sitemaps.py
+++ clothes/sitemaps.py
@@ -1,4 +1,7 @@
 from django.contrib import sitemaps
+from django.urls import reverse
+
+
 class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
     changefreq = "daily"
@@ -6,3 +9,7 @@ class StaticViewSitemap(sitemaps.Sitemap):
     # This method returns a list of URL names (defined in your urls.py) that should be included in the sitemap.
     def items(self):
         return ["clothes:home_page", "clothes:men-clothes", "clothes:women-clothes", "clothes:contact"]
+
+    # The reverse(item) function uses this item to retrieve the actual URL associated with that URL name from your urls.
+    def location(self, item):
+        return reverse(item)  # item is one of the values from the list returned by items()
