ad18c4d70e4b7631d47d4b4481b13ce56d074105
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-07 01:44:41 +0330

    Check for POST request to handle form submissions

--- blog/views.py
+++ blog/views.py
@@ -30,6 +30,7 @@ def single_blog(request, pid):
     posts = Post.objects.filter(status='A', published_date__lte=timezone.now())
     post = get_object_or_404(posts, pk=pid)
     comments = Comment.objects.filter(approved=True, post=post.id)
+    if request.method == "POST":
     post.counted_view += 1
     post.save()
     contex = {'post': post,
