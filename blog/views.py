2174804ae770e4778664ee2a0aa24e3f2dfa6f5d
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-07 01:50:55 +0330

    Import messages from django.contrib

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
