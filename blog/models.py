9b99a833c45aa0b05c4d4688ae177a3ed0e95975
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-07 01:18:45 +0330

    Add email field in Comment model

--- blog/models.py
+++ blog/models.py
@@ -61,3 +61,4 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    email = models.EmailField()
