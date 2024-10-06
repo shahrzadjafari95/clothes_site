e9d90dd11c50186ed02fd225165aa32aeb4356fc
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-07 00:44:09 +0330

    Configure INTERNAL_IPS for Django Debug Toolbar access
    Add '127.0.0.1' to INTERNAL_IPS to allow the Django Debug Toolbar for local development

--- clothes_site/settings.py
+++ clothes_site/settings.py
@@ -189,3 +189,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# debug toolbar settings
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
