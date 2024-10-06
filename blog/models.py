a01cf9fc8a73f19137e7b27291a29b122ea2f165
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-07 01:18:54 +0330

    Add message field in Comment model

--- blog/models.py
+++ blog/models.py
@@ -62,3 +62,4 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.EmailField()
+    message = models.TextField()
