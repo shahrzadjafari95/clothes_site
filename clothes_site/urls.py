8ec751c397b8d0c9425a39969bd8c12eb168cf61
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-07 00:23:10 +0330

    Add URL routing for 'django_summernote' in urls.py with path('summernote/', include('django_summernote.urls'))

--- clothes_site/urls.py
+++ clothes_site/urls.py
@@ -34,8 +34,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
     path('robots.txt', include('robots.urls')),
-
-
+    path('summernote/', include('django_summernote.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
