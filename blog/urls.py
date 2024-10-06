325c0fbe9cec54bc224a8509e1759fbaf73f2dd1
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-07 00:54:16 +0330

    Define path('tag/<str:tag_name>/', views.blog_home, name='tag') to handle requests for specific tags
    Add URL path for tag-based filtering in blog

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ app_name = 'blog'
 urlpatterns = [path('', views.blog_home,  name='blog-home'),
                path('post-<int:pid>', views.single_blog, name='single-blog'),
                path('category/<str:cat_name>/', views.blog_home, name='category'),
+               path('tag/<str:tag_name>/', views.blog_home, name='tag'),
                path('author/<str:author>/', views.blog_home, name='author'),
 ]
