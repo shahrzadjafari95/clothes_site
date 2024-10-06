f87180f2f63b80b055e71b061e9845460bdf8287
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-07 01:20:41 +0330

    Add approved field to Comment model for comment moderation
    - Define 'approved' as a BooleanField with a default value of False to indicate whether a comment has been approved for display

--- blog/models.py
+++ blog/models.py
@@ -65,3 +65,5 @@ class Comment(models.Model):
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
