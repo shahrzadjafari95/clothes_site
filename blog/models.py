1c5939702e15ce9925e25dbf0a458f0ea8da5067
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-07 01:19:43 +0330

    Add updated_date field to Comment model for tracking modifications
    - Define 'updated_date' as a DateTimeField with auto_now=True to automatically update the timestamp whenever the comment is modified

--- blog/models.py
+++ blog/models.py
@@ -64,3 +64,4 @@ class Comment(models.Model):
     email = models.EmailField()
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
