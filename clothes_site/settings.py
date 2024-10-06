fdedcbd81b87dc461f8c8293c1b24395406d95c9
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-07 00:43:14 +0330

    Add 'debug_toolbar.middleware.DebugToolbarMiddleware' to MIDDLEWARE settings

--- clothes_site/settings.py
+++ clothes_site/settings.py
@@ -106,6 +106,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'clothes_site.urls'
