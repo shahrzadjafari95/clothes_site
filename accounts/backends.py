068237dd0e6b6c9526f400b0f4cb223aecb8a3d5
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-20 20:57:29 +0330

    Created `EmailOrUsernameModelBackend` to allow users to log in using either their email or username.

--- accounts/backends.py
+++ accounts/backends.py
@@ -3,3 +3,9 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
+class EmailOrUsernameModelBackend(ModelBackend):
+    """
+    Custom authentication backend to allow users to authenticate using either their email or username.
+    """
+
