27f427fa6a5a9c22b2adb1c7e803f4bf704ec5cc
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-23 00:48:39 +0330

    This is the specific URL path that users are directed to after successfully submitting their email for a password reset.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from accounts import views
 from django.contrib.auth.views import (
     PasswordResetView,
+    PasswordResetDoneView,
 
 app_name = 'accounts'
 
@@ -11,4 +12,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),  # signup(register) url
     path('password-reset/', PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
          name='password-reset'),
+
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
 
