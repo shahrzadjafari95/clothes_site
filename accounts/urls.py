f99fb5bfc9267d1e1012ef4848ee85bc0a0b171c
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-22 16:31:13 +0330

    Add register URL to accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from accounts import views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),  # login url
     path('logout/', views.logout_view, name='logout'),  # logout url
+    path('register/', views.register, name='register'),  # signup(register) url
