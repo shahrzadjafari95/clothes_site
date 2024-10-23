0564cd4fd28b0e014705cc513b3543daf3af4fd9
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-23 17:36:25 +0330

    Add password reset confirmation URL pattern
    - Added path for `password_reset_confirm` to handle password reset requests
    
    - Integrated `PasswordResetConfirmView` with a custom template `registration/password_reset_confirm.html`.
    
    - Ensures users can confirm and complete their password reset using the unique token and user ID (`uidb64` and `token`).

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from accounts import views
 from django.contrib.auth.views import (
     PasswordResetView,
     PasswordResetDoneView,
+    PasswordResetConfirmView,
 
 app_name = 'accounts'
 
@@ -15,4 +16,8 @@ urlpatterns = [
 
     path('password-reset/done/', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
          name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+         name='password_reset_confirm'),
 
