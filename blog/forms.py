31c3b1009e514b42eea2d31c407a72ec227fea47
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-12-06 22:40:00 +0330

    Created a `NewsletterForm` class using Django's `ModelForm` to manage `Newsletter` model data.

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,11 @@
 from django import forms
-from .models import Comment
+from .models import Comment, Newsletter
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'message']
+
+
+class NewsletterForm(forms.ModelForm):
