f12a3e5f88a90c21bbcb65738757669b795589bf
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-12-07 23:18:16 +0330

    Add maintenance mode handling and update URL patterns
    - Includes routes for admin, authentication, blog, sitemaps, robots.txt, and debugging tools.
    - Ensures proper handling of static and media files.

--- clothes_site/urls.py
+++ clothes_site/urls.py
@@ -28,20 +28,6 @@ sitemaps = {
     "blog": BlogSitemap,
 }
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('django.contrib.auth.urls')),
-    path('', include('clothes.urls')),
-    path('blog/', include('blog.urls')),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
-    path('robots.txt', include('robots.urls')),
-    path("__debug__/", include("debug_toolbar.urls")),
-    path('summernote/', include('django_summernote.urls')),
-    path('captcha/', include('captcha.urls')),  # captcha urls
-    path('accounts/', include('accounts.urls')),
-]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Define handler404 and handler500 for redirecting to maintenance
 if settings.MAINTENANCE_MODE:
     handler404 = 'clothes_site.views.maintenance_view'
@@ -55,3 +41,19 @@ if settings.MAINTENANCE_MODE:
     urlpatterns += [
         re_path(r'^.*$', views.maintenance_view),  # Catch-all URL
     ]
+else:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('', include('django.contrib.auth.urls')),
+        path('', include('clothes.urls')),
+        path('blog/', include('blog.urls')),
+        path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+        path('robots.txt', include('robots.urls')),
+        path("__debug__/", include("debug_toolbar.urls")),
+        path('summernote/', include('django_summernote.urls')),
+        path('captcha/', include('captcha.urls')),  # captcha urls
+        path('accounts/', include('accounts.urls')),
+    ]
+    # Add static and media files handling
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
