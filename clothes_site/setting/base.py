0e95f5bad97cab54e4ff7e1451ea5c6227ec713d
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-12-07 23:43:47 +0330

    Defined `STATIC_URL` and `STATIC_ROOT` for serving static files.

--- clothes_site/setting/base.py
+++ clothes_site/setting/base.py
@@ -5,3 +5,7 @@ MAINTENANCE_MODE = False  # Set to False when your site is live
 COMPRESS_ENABLED = True  # Enable Django Compressor
 COMPRESS_OFFLINE = True  # Enables offline compression (recommended for production)
 
+# Static files settings
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
