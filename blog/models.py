d0b2bdae5f68ad501707cd8d4eb84f8a451b344d
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-12-07 23:59:44 +0330

    Refactor image handling in save method: resizing, reformatting, and deletion of old images
    - Resizes uploaded images to 230x300px and converts them to JPEG format with 85% quality.
    - Deletes the old image if it's replaced by a new one during the update.
    - Handles both new image uploads and image updates, including error logging in case of failure.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from django.contrib.auth.models import User
+from django.core.files.storage import default_storage
 from django.db import models
 from django.urls import reverse
 from io import BytesIO
@@ -36,17 +37,41 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)  # when the object first created,set current date and time
     update_date = models.DateTimeField(auto_now=True)  # when the object updated, save current date and time
 
-    def save(self):  # this method resized all image
-        im = Image.open(self.img)  # Opening the uploaded image
-        output = BytesIO()
-        im = im.resize((700, 353))  # Resize/modify the image
-        im.save(output, format='JPEG', quality=100)  # after modifications, save it to the output
-        output.seek(0)
-        # change the imagefield value to be the newly modified image value
-        self.img = InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % self.img.name.split('.')[0], 'image/jpeg',
-                                        sys.getsizeof(output), None)
+    def save(self, *args, **kwargs):
+        try:
+            # Check if an instance already exists to handle old image deletion
+            old_instance = Post.objects.get(pk=self.pk)
+            if old_instance.img and old_instance.img != self.img:
+                old_instance.img.delete(save=False)
+        except Post.DoesNotExist:
+            # Instance doesn't exist; this is a new object
+            pass
 
-        super(Post, self).save()
+        # Resize image only if it's a new upload
+        if self.img and not default_storage.exists(self.img.name):
+            im = Image.open(self.img)
+            output = BytesIO()
+
+            # Convert image mode to RGB if necessary
+            if im.mode in ("RGBA", "P"):
+                im = im.convert("RGB")
+
+            # Resize the image
+            im = im.resize((700, 353))
+            im.save(output, format='JPEG', quality=85)
+            output.seek(0)
+
+            # Replace the image with the resized one
+            self.img = InMemoryUploadedFile(
+                output,
+                'ImageField',
+                f"{self.img.name.split('.')[0]}.jpg",
+                'image/jpeg',
+                sys.getsizeof(output),
+                None
+            )
+
+        super(Post, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['-created_date']
--- clothes/models.py
+++ clothes/models.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 import sys
 from PIL import Image
+from django.core.files.storage import default_storage
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
 
@@ -32,17 +33,45 @@ class All_Type_Clothes(models.Model):
     update_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(null=True)
 
-    def save(self):  # this method resized all image
-        im = Image.open(self.image)  # Opening the uploaded image
-        output = BytesIO()
-        im = im.resize((230, 300))  # Resize/modify the image
-        im.save(output, format='JPEG', quality=100)  # after modifications, save it to the output
-        output.seek(0)
-        # change the imagefield value to be the newly modified image value
-        self.img = InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % self.image.name.split('.')[0], 'image/jpeg',
-                                        sys.getsizeof(output), None)
+    def save(self, *args, **kwargs):
+        # Handle deletion of the old image
+        if self.pk:  # Only check for old instance if this is an update
+            try:
+                old_instance = All_Type_Clothes.objects.get(pk=self.pk)
+                if old_instance.image and old_instance.image != self.image:
+                    old_instance.image.delete(save=False)
+            except All_Type_Clothes.DoesNotExist:
+                pass
 
-        super(All_Type_Clothes, self).save()
+        # Resize image only if it's a new upload or updated image
+        if self.image and hasattr(self.image, 'file'):
+            try:
+                im = Image.open(self.image.file)
+                output = BytesIO()
+
+                # Convert image to RGB if necessary
+                if im.mode in ("RGBA", "P"):
+                    im = im.convert("RGB")
+
+                # Resize the image to the desired dimensions
+                im = im.resize((230, 300))
+                im.save(output, format='JPEG', quality=85)
+                output.seek(0)
+
+                # Replace the image with the resized one
+                self.image = InMemoryUploadedFile(
+                    output,
+                    'ImageField',
+                    f"{self.image.name.split('.')[0]}.jpg",
+                    'image/jpeg',
+                    sys.getsizeof(output),
+                    None
+                )
+            except Exception as e:
+                print(f"Error processing image: {e}")  # Log the error
+
+        # Call the parent class's save method
+        super(All_Type_Clothes, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.gender
