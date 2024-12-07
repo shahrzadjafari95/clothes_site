48843c32b6962f31b2a2ac73cadcb35a48f70410
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-12-07 23:43:07 +0330

    Enabled Django Compressor with `COMPRESS_ENABLED` and `COMPRESS_OFFLINE` settings for efficient CSS/JS.

--- clothes_site/setting/base.py
+++ clothes_site/setting/base.py
@@ -1,3 +1,7 @@
 # base.py
 MAINTENANCE_MODE = False  # Set to False when your site is live
 
+
+COMPRESS_ENABLED = True  # Enable Django Compressor
+COMPRESS_OFFLINE = True  # Enables offline compression (recommended for production)
+
