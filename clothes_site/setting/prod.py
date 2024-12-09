e667806d0f0c659773abf6bfce1481d8eb7dcff9
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-12-09 22:04:36 +0330

    Added MySQL database configuration for production.

--- clothes_site/setting/prod.py
+++ clothes_site/setting/prod.py
@@ -7,3 +7,19 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['www.best-blog-travel.ir', 'best-blog-travel.ir']
 
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+
+# Database in CPANEL for production
+DATABASES = {
+    'default': {
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.mysql'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),  # Use 'localhost' by default
+        'PORT': config('DB_PORT', cast=int, default=3306),  # Use 3306 by default
+    }
+}
+
